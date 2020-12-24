22bc94a00f302d5b087b0d1dcebf7ea54641d003
merajV <meraj.sheikhhaa@gmail.com>
2020-12-24 13:41:29 +0330

    updated v0.1

---
+++ QueryBuilder/src/main/java/com/github/querybuilder/QueryBuilder.kt
@@ -0,0 +1,256 @@
+package com.github.querybuilder
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+
+
+class QueryBuilder(private val context: Context, private var DATABASE_NAME: String){
+    private var db:SQLiteDatabase
+    init {
+        if (!DATABASE_NAME.endsWith(".db")){
+            DATABASE_NAME += ".db";
+        }
+        DATABASE_NAME = DATABASE_NAME.replace(" ", "_")
+        db = context.openOrCreateDatabase(
+            DATABASE_NAME,
+            Context.MODE_PRIVATE,
+            null
+        )
+    }
+    //
+    private var CursorLists:MutableList<Cursor> = ArrayList()
+    private var preparedStatements :MutableList<String> = ArrayList()
+    // Query
+    private var QUERY :String = ""
+    private fun getQueryBuilder(){
+        if(SELECT == "*"){
+            QUERY = "SELECT * "
+        }else{
+            QUERY = SELECT
+        }
+        QUERY += "FROM $TABLE_NAME "
+        if(Where != ""){
+            QUERY += Where
+        }
+        if(OrderByColumnName != null){
+           QUERY += "ORDER BY $OrderByColumnName $ORDER "
+        }
+
+        if(LimitQuery != ""){
+            QUERY += LimitQuery
+        }
+        selectColumns = ""
+        SELECT = "*"
+        Where = ""
+    }
+    // Table
+    private lateinit var TABLE_NAME :String
+    fun table(TableName: String): QueryBuilder {
+        TABLE_NAME = TableName
+        return this
+    }
+    // Select
+    private var SELECT = "*"
+    private var selectColumns = ""
+    fun select(Select: Array<String>): QueryBuilder {
+        Select.forEach {
+            selectColumns += "$it,"
+        }
+        SELECT = "SELECT " + selectColumns.substring(0, selectColumns.length - 1) + " "
+        return this
+    }
+    fun select(Select: String): QueryBuilder {
+        selectColumns = Select
+        SELECT = "SELECT $Select "
+        return this
+    }
+
+    fun andSelect(Select: Array<String>): QueryBuilder {
+        Select.forEach {
+            selectColumns += "$it,"
+        }
+        SELECT = "SELECT " + selectColumns.substring(0, selectColumns.length - 1) + " "
+        return this
+    }
+    fun andSelect(Select: String): QueryBuilder {
+        selectColumns += Select
+        SELECT = "SELECT $selectColumns "
+        return this
+    }
+    // Where
+    private var Where : String? = ""
+    fun where(Selection: String, SelectionArg: String): QueryBuilder {
+        if(Where != ""){
+            Where += "AND $Selection = ? "
+            preparedStatements.add(SelectionArg)
+        }else{
+            Where = "where $Selection = ? "
+            preparedStatements.add(SelectionArg)
+        }
+        return this
+    }
+    fun where(Selection: String,operation :String, SelectionArg: String): QueryBuilder {
+        if(Where != ""){
+            Where += "AND $Selection $operation ? "
+            preparedStatements.add(SelectionArg)
+        }else{
+            Where = "where $Selection $operation ? "
+            preparedStatements.add(SelectionArg)
+        }
+        preparedStatements.add(SelectionArg)
+        return this
+    }
+    fun whereBetween(Selection: String, from: String ,to:String): QueryBuilder {
+        var before = "where"
+        if(Where != ""){
+            before = "AND"
+        }
+        Where += "$before $Selection BETWEEN ? AND ? "
+        preparedStatements.add(from)
+        preparedStatements.add(to)
+        return this
+    }
+    fun whereNotBetween(Selection: String, from: String ,to:String): QueryBuilder {
+        var before = "where"
+        if(Where != ""){
+            before = "AND"
+        }
+        Where += "$before $Selection NOT BETWEEN ? AND ? "
+        preparedStatements.add(from)
+        preparedStatements.add(to)
+        return this
+    }
+    fun orWhere(Selection: String, SelectionArg: String): QueryBuilder {
+        Where += "OR $Selection = ? "
+        preparedStatements.add(SelectionArg)
+        return this
+    }
+    // Limit And Offset
+    private var LimitQuery :String = ""
+    fun limit(Limit:Int, Offset: Int = 0): QueryBuilder {
+        LimitQuery = "LIMIT $Limit "
+        if(Offset != 0){
+            LimitQuery += "OFFSET $Offset "
+        }
+        return this
+    }
+    // OrderBy
+    private var OrderByColumnName: String? = null
+    private var ORDER:String? = null
+    fun orderBy(ColumnName: String,Order:String = "DESC"): QueryBuilder {
+        OrderByColumnName = ColumnName
+        ORDER = Order
+        return this
+    }
+    // Count
+    @SuppressLint("Recycle")
+    fun count(): Int {
+        getQueryBuilder()
+        val db:SQLiteDatabase = context.openOrCreateDatabase(
+            DATABASE_NAME,
+            Context.MODE_PRIVATE,
+            null
+        )
+        var cursor : Cursor? = null
+        try{
+            cursor = db.rawQuery(QUERY, preparedStatements.toTypedArray())
+        }catch (e:Exception){
+            return 0
+        }
+        val rowsCount:Int = cursor.count
+        db.close()
+        QUERY = ""
+        preparedStatements = ArrayList()
+        cursor.close()
+        return rowsCount
+    }
+    // First
+    @SuppressLint("Recycle")
+    fun first(): Cursor? {
+        getQueryBuilder()
+        val db:SQLiteDatabase = context.openOrCreateDatabase(
+            DATABASE_NAME,
+            Context.MODE_PRIVATE,
+            null
+        )
+        val cursor = db.rawQuery(QUERY, preparedStatements.toTypedArray())
+
+        QUERY = ""
+        preparedStatements = ArrayList()
+        return if(cursor != null && cursor.moveToFirst() ) {
+            CursorLists.add(cursor)
+            cursor
+        }else{
+            cursor?.close()
+            null
+        }
+    }
+    // Get
+    fun get(): Cursor? {
+        getQueryBuilder()
+        val cursor = db.rawQuery(QUERY, preparedStatements.toTypedArray())
+        QUERY = ""
+        preparedStatements = ArrayList()
+        CursorLists.add(cursor)
+        return cursor
+    }
+    // Insert
+    fun insert(ColumnName: Array<String>, ColumnData: Array<String>): Boolean {
+        db.beginTransaction()
+        QUERY = "INSERT INTO $TABLE_NAME ("
+        var columns = ""
+        ColumnName.forEach {
+            columns += "$it,"
+        }
+        columns = columns.substring(0, columns.length - 1)
+        QUERY += "$columns) VALUES ("
+        var columndataString = ""
+        ColumnData.forEach {
+            columndataString += "?,"
+        }
+        columndataString = columndataString.substring(0, columndataString.length - 1)
+        QUERY += "$columndataString)"
+        val stmt = db.compileStatement(QUERY)
+        var forEachIndex = 1
+        ColumnData.forEach {
+            stmt.bindString(forEachIndex, it)
+            forEachIndex++
+        }
+
+        stmt.executeInsert()
+        stmt.close()
+        try{
+            db.setTransactionSuccessful()
+        }catch (e :Exception){
+            db.endTransaction()
+            return false
+        }
+        db.endTransaction()
+        return true
+    }
+    // Exist And does not exist
+    fun exists(): Boolean {
+    getQueryBuilder()
+        return this.count() > 0
+    }
+    fun doesntExist(): Boolean {
+        getQueryBuilder()
+        return this.count() == 0
+    }
+    // Open And Close Connection
+    fun open(){
+        db = context.openOrCreateDatabase(
+            DATABASE_NAME,
+            Context.MODE_PRIVATE,
+            null
+        )
+    }
+    fun close(){
+        CursorLists.forEach {
+            it.close()
+        }
+        db.close()
+    }
+}
